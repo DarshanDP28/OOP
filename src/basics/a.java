9c3c5c3cc2a1f4f22c042e3ee1b43386bbd951c4
darshan padmane <DARSHANPADMANEDP@GMAIL.COM>
2024-03-05 20:12:25 +0530

    Update a.java

--- src/basics/a.java
+++ src/basics/a.java
@@ -1,29 +1,21 @@
 package basics;
 
  public class a{
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("from a");
-
 	}
-
 }
  class b{
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			System.out.println("from b");
-
 		}
-
 	}
  class c{
-
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			System.out.println("from c");
-
 		}
-
 	}
